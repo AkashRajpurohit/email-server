e1569bb9d88a306448b4e0321fdbe08044cbbc98
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-06-15 22:03:44 +0530

    feat: :sparkles: post endpoint token check for authorization

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 
-const app = new Hono();
+type Bindings = {
+  TOKEN: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
 
 // Middlewares
 app.use('*', cors());
 
 // Routes
 app.get('/', (c) => {
-  return c.redirect(
-    'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-    302
-  );
+  return c.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ', 302);
+});
+
+app.post('/send', (c) => {
+  const { TOKEN } = c.env;
+  const authToken = c.req.headers.get('Authorization');
+
+  if (authToken !== TOKEN) {
+    return c.json({ success: false, message: 'Missing authorization' });
+  }
+
+  return c.json({ success: true });
 });
 
 app.notFound((c) => c.json({ ok: false, message: 'Not Found' }, 404));
