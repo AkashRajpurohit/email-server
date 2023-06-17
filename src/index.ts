3d3113bbb44d6ca2adb6bde4a6969ce244df9cc6
AkashRajpurohit <akashrajpurohit.009@gmail.com>
2023-06-17 09:07:05 +0530

    feat: :sparkles: error handling improved

--- src/index.ts
+++ src/index.ts
@@ -23,10 +23,20 @@ app.post('/send', authMiddleware(), async (c) => {
     return c.json({ success: false, issues: result.error.flatten() }, 400);
   }
 
-  const response = await sendEmail(email);
-  return c.json(response, response.success ? 200 : 400);
+  try {
+    await sendEmail(email);
+  } catch (err) {
+    return c.json({ success: false, message: err.message }, 400);
+  }
+
+  return c.json({ success: true, message: 'Emails sent' }, 200);
 });
 
 app.notFound((c) => c.json({ ok: false, message: 'Not Found' }, 404));
+
+app.onError((err, c) => {
+  console.error(err);
+  return c.json({ success: false, message: err.message }, 500);
+});
 
 export default app;
